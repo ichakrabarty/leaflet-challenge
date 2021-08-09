bf10ebd54eb8350e6fc3deacddd1b2ce941f2a46
BadHacker <ishan.chak@gmail.com>
2021-08-09 15:10:10 -0500

    redo code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,91 +1,53 @@
-let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-d3.json(url).then(function(data){
-    console.log(data.features)   
-    createFeatures(data.features);
-});
+ const equrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-function createFeatures(EQdata){
-     
-    let earthquakes = [];
-    for (i=0;EQdata.length;i++){
-       earthquakes.push( 
-           L.geoJSON(EQdata[i]).bindPopup(`<h2> ${EQdata[i].properties.place} </h2> <p> ${Date(EQdata[i].properties.time)} </p>`)
-            
-       );
-   }
-     
-    // function onEachFeature(feature, layer) {
-    //     layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-    // }
-
-    // var earthquakes = L.geoJSON(EQdata, {
-    //     onEachFeature: onEachFeature
-    //   });
-
-    function magnitudeSize(mag){
-        return mag * 500
-    }
-
-    function magnitudeDepth(depth){
-        if (depth > 1 && depth <= 1.9)
-            return "ffff00";
-        if (depth > 1.9 && depth <= 2.9)
-            return "ffe600"  ;  
-        if (depth > 2.9 && depth <= 3.9)
-            return "ffcc00";
-        if (depth > 3.9 && depth <= 4.9)
-            return "ffb300";
-        if (depth > 4.9 && depth <= 5.9)
-            return "ff9900";
-        if (depth > 5.9 && depth <= 6.9)
-            return "ff8000";
-        if (depth > 6.9 && depth <= 7.9)
-            return "fff660";
-        if (depth > 7.9 && depth <= 8.9)
-            return "ff4c00";  
-        if (depth > 8.9)
-            return "ff3300";     
-    }
-
-    L.circle({
-        radius: magnitudeSize(earthquakes.properties.mag),
-        color:  magnitudeDepth(earthquakes.geometry.coordinates[2])
-    })
-
-    let EQLayer = L.layerGroup(earthquakes)
-
-    createMap(EQLayer);
-}
-
-function createMap(EQlayer) {
-    
-
-    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        });
-
-    
-
-    let baseMaps = {
-        "Street Map": street
-    }
-
-    let overlayMaps = {
-        Earthquakes: earthquakes
-      };
-
-    let myMap = L.map("map", {
+ let myMap = L.map("map", {
         center: [45.52, -122.67],
         zoom: 5,
-        layers: street
-      });
+        });
 
-    L.control.layers(baseMaps, overlayMaps, {
-     collapsed: false
-    }).addTo(myMap);
-      
-}
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(myMap); 
 
 
- 
+
+d3.json(equrl).then(data => {
+        console.log(data.features)
+        
+        function magnitudeSize(mag){
+            return mag * 10000
+        }
+                    
+        function magnitudeDepth(depth){
+            if (depth >= 1 && depth < 2)
+                return "ffff00";
+            if (depth >= 2 && depth < 3)
+                return "ffe600"  ;  
+            if (depth >= 3 && depth < 4)
+                return "ffcc00";
+            if (depth >= 4 && depth < 5)
+                return "ffb300";
+            if (depth >= 5 && depth < 6)
+                return "ff9900";
+            if (depth >= 6 && depth < 7)
+                return "ff8000";
+            if (depth >= 7 && depth < 8)
+                return "fff660";
+            if (depth >= 8 && depth < 9)
+                return "ff4c00";  
+            if (depth >= 9)
+                return "ff3300";     
+        }
+
+        for (i=0;i<data.features.length;i++){
+            L.circle([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]], {
+                        radius: magnitudeSize(data.features[i].properties.mag),
+                        color:  magnitudeDepth(data.features[i].geometry.coordinates[2])
+                    }).bindPopup("<h2>" + data.features[i].properties.place + "</h2>" + "<p>" + Date(data.features[i].properties.time) + "</p>").addTo(myMap)
+        }
+
+       
+
+});
+
